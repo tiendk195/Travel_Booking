94257778179ca9f13e532af1b79bac8db790b65a
tiendk195 <tiendk195@gmail.com>
2024-04-02 21:17:47 +0700

    Add Search Bar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -34,7 +34,14 @@ const Header = () => {
               <ul className="menu d-flex align-items-center gap-5">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
-                    <NavLink to={item.path}>{item.display}</NavLink>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
                   </li>
                 ))}
               </ul>
---
+++ frontend/src/shared/Subtitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Subtitle = ({ subtitle }) => {
+  return (
+    <h3>
+      <div className="section__subtitle">{subtitle}</div>
+    </h3>
+  );
+};
+
+export default Subtitle;
