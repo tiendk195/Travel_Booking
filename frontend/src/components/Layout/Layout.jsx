c440a29f91b4839944167d80e08556f6333a20cd
tiendk195 <tiendk195@gmail.com>
2024-04-02 20:02:11 +0700

    Build base front-end react

---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./../Header/Header";
+import Footer from "./../Footer/Footer";
+import Router from "../../router/Routers";
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Router />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
