13b90133f5a7fe451ed0dca9e9d5c5162fd97de0
tiendk195 <tiendk195@gmail.com>
2024-04-03 11:52:19 +0700

    Add Tour Page

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CommonSection from "../shared/CommonSection";
+import "../styles/tour.css";
+import tourData from "../assets/data/tours";
+
+import { Container, Row, Col } from "reactstrap";
+import SearchBar from "./../shared/SearchBar";
+import TourCard from "./../shared/TourCard";
+import Newsletter from "./../shared/Newsletter";
 
 const Tours = () => {
-  return <div>Tours</div>;
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setpage] = useState(0);
+
+  useEffect(() => {
+    const pages = Math.ceil(5 / 4);
+    setPageCount(pages);
+  }, [page]);
+
+  return (
+    <>
+      <CommonSection title={"All Tours"} />
+      <section className="pt-0">
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" className="mb-4" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))}
+
+            <Col lg="12">
+              <div className="pagination d-flex align-items-center justify-content-center gap-3 mt-4">
+                {[...Array(pageCount).keys()].map((number) => (
+                  <span
+                    key={number}
+                    onClick={() => setpage(number)}
+                    className={page === number ? "active__page" : ""}
+                  >
+                    {number + 1}
+                  </span>
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Newsletter />
+    </>
+  );
 };
 
 export default Tours;
---
+++ frontend/src/shared/CommonSection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./common-section.css";
+import { Container, Row, Col } from "reactstrap";
+const CommonSection = ({ title }) => {
+  return (
+    <section className="common__section">
+      <Container>
+        <Row>
+          <Col log="12">
+            <h1>{title}</h1>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default CommonSection;
